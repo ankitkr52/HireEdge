d5df71ef6e01ea97e915699023790eac0a734a05
ankitkr52 <ankit72p@gmail.com>
2026-05-11 19:14:51 +0530

    create auth controller for user login and sign up

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,14 +1,28 @@
-const userModel=require("../models/usermodels")
+const userModel = require("../models/usermodels")
+const bcrypt=require("bcrypt")
+const jwt=require("jsonwebtoken")
 
-
-await function registerUserController(req,res){
-    const {username,email,password}=req.body
-    if(!username ||!email ||!password){
-        return res.status(400).json({message="Please provide username, email and password" })
+await function registerUserController(req, res) {
+    const { username, email, password } = req.body
+    if (!username || !email || !password) {
+        return res.status(400).json({ message="Please provide username, email and password" })
     }
 
 }
+const isUserAlreadyExist = await userModel.findOne({
+    $or: [{ username }, { email }]
+})
+if (isUserAlreadyExist) {
+    return res.status(400).json({ message: "user already exists with this username or email" })
+}
+const user=new userModel({
+    username,
+    email,
+    password:hash
+})
 
-module.exports={
+const token=
+
+module.exports = {
     registerUserController
 }
