4900f15a2f59e6e5ffc91d7a05b7314d37d7edc7
ankitkr52 <ankit72p@gmail.com>
2026-05-13 22:38:23 +0530

    update app.js file

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,7 @@
 const express = require('express');  // Fixed typo
 
 const app = express();
-app.use(express.json());
+ app.use(express.json());
 
 // require all the routes here
 const authRouter = require('./routes/auth.routes');
