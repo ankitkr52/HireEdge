141339c1778b2135b4054c783cb5b22cb96a4a60
ankitkr52 <ankit72p@gmail.com>
2026-05-25 16:04:51 +0530

    feat: setup project foundation with auth system

---
+++ Frontend/src/features/auth/services/auth.api.js
@@ -0,0 +1,44 @@
+import axios from 'axios'
+
+const BASE_URL = 'http://localhost:3000/api/auth'
+const config = { withCredentials: true }
+
+export async function register({ username, email, password }) {
+    try {
+        const response = await axios.post(`${BASE_URL}/register`, {
+            username, email, password
+        }, config)
+        return response.data
+    } catch (err) {
+        throw err  // ✅
+    }
+}
+
+export async function login({ email, password }) {
+    try {
+        const response = await axios.post(`${BASE_URL}/login`, {
+            email, password
+        }, config)
+        return response.data
+    } catch (err) {
+        throw err  // ✅
+    }
+}
+
+export async function logout() {
+    try {
+        const response = await axios.get(`${BASE_URL}/logout`, config)
+        return response.data
+    } catch (err) {
+        throw err  // ✅
+    }
+}
+
+export async function getMe() {
+    try {
+        const response = await axios.get(`${BASE_URL}/get-me`, config)  // ✅ = → :
+        return response.data
+    } catch (err) {
+        throw err  // ✅
+    }
+}
