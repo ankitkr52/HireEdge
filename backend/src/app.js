718452971289adfa762a8292734488f9e1ff91ed
ankitkr52 <ankit72p@gmail.com>
2026-08-04 08:47:23 +0530

    working on deployment

--- backend/src/app.js
+++ backend/src/app.js
@@ -18,6 +18,7 @@ const interviewRouter=require('./routes/interview.routes')
 
 
 // using all the routes here
+app.get('/', (req, res) => res.send("API is working here"))
 app.use('/api/auth', authRouter);
 app.use('/api/interview',interviewRouter)
 module.exports = app;
