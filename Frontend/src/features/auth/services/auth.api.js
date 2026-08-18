7ae936b344682ecb0b397eb2debdd432d5462cec
ankitkr52 <ankit72p@gmail.com>
2026-08-18 12:01:27 +0530

    connect frontend with production backend

--- Frontend/src/features/auth/services/auth.api.js
+++ Frontend/src/features/auth/services/auth.api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const BASE_URL = 'http://localhost:3000/api/auth'
+const BASE_URL = `${import.meta.env.VITE_API_URL}/api/auth`
 const config = { withCredentials: true }
 
 export async function register({ username, email, password }) {
